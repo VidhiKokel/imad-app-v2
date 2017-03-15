786d60a88c17a33782a3c6ae55af8113684a84ba
VidhiKokel <15ce053@charusat.edu.in>
2017-03-15 13:24:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element = document.getElementById('mytxt');
 element.innerHTML = 'Heya';
 
 var img = document.getElementById('dragon');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
+    var interval = setInterval(moveRight,100)
   img.style.marginLeft = '100px';
 };
